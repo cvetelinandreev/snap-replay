0f71a540d2a9b00da7f96bd3fb5f775f67ce7d8d
cvetelinandreev <cvetelin.andreev@gmail.com>
2022-02-10 16:57:25 +0200

    revert DB_LOG

--- src/js/playback.js
+++ src/js/playback.js
@@ -13,7 +13,7 @@ window.Trace = {
 
 export class Playback {
 
-    static DB_LOG = false;
+    static DB_LOG = true;
 
     // 0 because we do this in the script itself now
     static BUFFER_MS = 0;
