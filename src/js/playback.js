367da5370ed5355a2f4d97f088efc75d566d4af4
cvetelinandreev <cvetelin.andreev@gmail.com>
2022-02-10 17:38:37 +0200

    stop DB logging and skip finish modal

--- src/js/playback.js
+++ src/js/playback.js
@@ -13,7 +13,7 @@ window.Trace = {
 
 export class Playback {
 
-    static DB_LOG = true;
+    static DB_LOG = false;
 
     // 0 because we do this in the script itself now
     static BUFFER_MS = 0;
@@ -508,7 +508,7 @@ export class Playback {
             this.checkForFocus();
         }, 1);
         if (duration > this.duration - 0.1) {
-            this.showFinishedModal();
+            // this.showFinishedModal();
             this.pause();
         }
     }
